ce9afe541d8d79a2773fe141e7f6b71f2a11782f
DEEPESH KUMAR <dipeshkumar@Deepeshs-MacBook-Air.local>
2026-02-04 19:41:02 +0530

    add password based private room chat

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
 const express = require("express");
 const http = require("http");
-const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// serve static files
-app.use(express.static(path.join(__dirname, "public")));
-
-// force home route
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  socket.on("encrypted-message", (data) => {
-    socket.broadcast.emit("encrypted-message", data);
+  console.log("User connected");
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    socket.room = room;
+    socket.to(room).emit("status", "online");
+  });
+
+  socket.on("message", (msg) => {
+    if (socket.room) {
+      socket.to(socket.room).emit("message", msg);
+    }
+  });
+
+  socket.on("typing", () => {
+    if (socket.room) {
+      socket.to(socket.room).emit("typing");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    if (socket.room) {
+      socket.to(socket.room).emit("status", "offline");
+    }
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running on port", PORT);
 });
