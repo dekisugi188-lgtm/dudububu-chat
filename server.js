5978ccc0eebd3392fab504780bcf243910664486
DEEPESH KUMAR <dipeshkumar@Deepeshs-MacBook-Air.local>
2026-02-04 19:18:32 +0530

    fix cannot GET root route

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// serve frontend
-app.use(express.static("public"));
+// serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// force home route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 io.on("connection", (socket) => {
   socket.on("encrypted-message", (data) => {
