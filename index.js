079cecb9509def7d6b8deb480a56381b52d3aac3
DEEPESH KUMAR <dipeshkumar@Deepeshs-MacBook-Air.local>
2026-02-04 16:36:30 +0530

    Final working chat app

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// serve files from /public folder
-app.use(express.static("public"));
+// serve frontend
+app.use(express.static(path.join(__dirname, "public")));
 
+// root route fix (IMPORTANT)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// socket logic
 io.on("connection", (socket) => {
+  console.log("User connected");
+
   socket.on("encrypted-message", (data) => {
-    // forward encrypted message to others
     socket.broadcast.emit("encrypted-message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
 
+// Render port
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log("Server running on http://localhost:" + PORT);
+  console.log("Server running on port", PORT);
 });
