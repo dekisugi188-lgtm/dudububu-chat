4644b5e34bd15a254ecadfca15d482b532ff5c73
DEEPESH KUMAR <dipeshkumar@Deepeshs-MacBook-Air.local>
2026-02-04 21:47:39 +0530

    fix mobile ui, online status, simple video call

--- server.js
+++ server.js
@@ -14,34 +14,23 @@ io.on("connection", (socket) => {
   socket.on("join-room", (password) => {
     room = password;
     socket.join(room);
-    socket.to(room).emit("user-online");
+    socket.to(room).emit("status", "online");
   });
 
-  socket.on("typing", () => {
-    socket.to(room).emit("typing");
-  });
+  socket.on("typing", () => socket.to(room).emit("typing"));
+  socket.on("stop-typing", () => socket.to(room).emit("stop-typing"));
 
-  socket.on("stop-typing", () => {
-    socket.to(room).emit("stop-typing");
-  });
+  socket.on("message", (msg) => socket.to(room).emit("message", msg));
+  socket.on("media", (data) => socket.to(room).emit("media", data));
 
-  socket.on("message", (msg) => {
-    socket.to(room).emit("message", msg);
-  });
-
-  socket.on("media", (data) => {
-    socket.to(room).emit("media", data);
-  });
-
-  // WebRTC signaling
-  socket.on("offer", (data) => socket.to(room).emit("offer", data));
-  socket.on("answer", (data) => socket.to(room).emit("answer", data));
-  socket.on("ice-candidate", (data) =>
-    socket.to(room).emit("ice-candidate", data)
-  );
+  // SIMPLE video call signaling (previous style)
+  socket.on("call-offer", (data) => socket.to(room).emit("call-offer", data));
+  socket.on("call-answer", (data) => socket.to(room).emit("call-answer", data));
+  socket.on("ice", (data) => socket.to(room).emit("ice", data));
+  socket.on("end-call", () => socket.to(room).emit("end-call"));
 
   socket.on("disconnect", () => {
-    if (room) socket.to(room).emit("user-offline");
+    if (room) socket.to(room).emit("status", "offline");
   });
 });
 
