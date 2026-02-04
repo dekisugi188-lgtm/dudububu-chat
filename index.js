78a493dab2f4d5749b75505b1af3a935883c3742
DEEPESH KUMAR <dipeshkumar@Deepeshs-MacBook-Air.local>
2026-02-04 15:43:21 +0530

    Initial deploy

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// serve files from /public folder
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  socket.on("encrypted-message", (data) => {
+    // forward encrypted message to others
+    socket.broadcast.emit("encrypted-message", data);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server running on http://localhost:" + PORT);
+});
